bb3bae510526b2e91a80bd0137adea9eae6f71c9
Enzo Peixoto <enzopeixoto043@gmail.com>
2023-08-27 19:01:01 -0300

    Check logic

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,37 +2,37 @@ package boardgame;
 
 public class Position {
 
-    private Integer row;
-    private Integer column;
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 
-    public Position(Integer row, Integer column) {
-        this.row = row;
-        this.column = column;
-    }
+	public int getRow() {
+		return row;
+	}
 
-    public Integer getRow() {
-        return row;
-    }
+	public void setRow(int row) {
+		this.row = row;
+	}
 
-    public void setRow(Integer row) {
-        this.row = row;
-    }
+	public int getColumn() {
+		return column;
+	}
 
-    public Integer getColumn() {
-        return column;
-    }
-
-    public void setColumn(Integer column) {
-        this.column = column;
-    }
-
-    public void setValues(Integer row, Integer column){
-        this.row = row;
-        this.column = column;
-    }
-
-    @Override
-    public String toString() {
-        return row + ", " + column;
-    }
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
 }
